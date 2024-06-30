24d9ba20bfda55318b64e93f404ca91c8df3907b
Garv2003 <aggarwalgarv0505@gmail.com>
2024-06-30 23:02:33 +0530

    COMPELETE NEXTJS PROJECT

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,18 @@
 import { Tabs } from "@/components";
+import { Suspense } from "react";
 
 const Home = () => {
-  return <Tabs />;
+  return (
+    <Suspense
+      fallback={
+        <div className="flex justify-center items-center h-50 w-full">
+          Loading...
+        </div>
+      }
+    >
+      <Tabs />
+    </Suspense>
+  );
 };
 
 export default Home;
